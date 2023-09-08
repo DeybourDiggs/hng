117dd4d993ff467f6b1b24bf85d45496476319d6
DeybourDiggs <ceric6764@gmail.com>
2023-09-08 00:34:48 -0700

    first commit

--- app.js
+++ app.js
@@ -10,9 +10,27 @@ app.get('/api',  (req, res) => {
 
     const currentDate = new Date();
     const currentDay = currentDate.toLocaleString('en-US', {weekday: 'long'})
-    const utcTime = currentDate.toISOString()
+    const utcTime = currentDate.toISOString();
+
+
+    const githubRepoUrl = 'https://github.com/DeybourDiggs/hng';
+    const githubFileUrl = `${githubRepoUrl}/blob/main/file_name.ext`;
+
+    const jsonResponse ={
+        slack_name,
+        current_day: currentDay,
+        utc_time: utcTime,
+        track,
+        github_file_url: githubRepoUrl,
+        github_source_url: githubFileUrl,
+        status_code: 200
+    }
+
+    res.json(jsonResponse)
 
 })
 
 
-app.listen(PORT);
+app.listen(PORT, ()=> {
+    console.log(`server is running on ${PORT}`)
+});
